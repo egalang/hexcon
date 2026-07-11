00ac39963ad70326a7df042fe13f3d21617d318a
Erwin Galang <erwing.geo@yahoo.com>
2026-07-11 16:55:20 +0800

    added pre-loader

--- src/main.ts
+++ src/main.ts
@@ -398,13 +398,162 @@ const COLORS = {
   text: '#ffffff',
 };
 
-class MenuScene extends Phaser.Scene {
+class BootScene extends Phaser.Scene {
   constructor() {
-    super('MenuScene');
+    super("BootScene");
   }
 
   preload() {
+    this.cameras.main.setBackgroundColor("#163d5d");
+
+    // Load ONLY the logo
+    this.load.image("game-logo", "/assets/logo.png");
+  }
+
+  create() {
+    this.cameras.main.fadeIn(200);
+
+    this.scene.start("PreloaderScene");
+  }
+}
+
+class PreloaderScene extends Phaser.Scene {
+  private progressBar!: Phaser.GameObjects.Graphics;
+
+  constructor() {
+    super("PreloaderScene");
+  }
+
+  preload() {
+    this.cameras.main.setBackgroundColor("#000000");
+
+    const centerX = this.cameras.main.centerX;
+    const centerY = this.cameras.main.centerY;
+
+    //
+    // Main loading UI container
+    //
+    const ui = this.add.container(centerX, centerY);
+
+    //
+    // Logo
+    //
+    const logo = this.add.image(0, -170, "game-logo")
+      .setOrigin(0.5)
+      .setScale(0.35);
+
+    //
+    // Title
+    //
+    const title = this.add.text(
+      0,
+      10,
+      "HEX CONQUEST",
+      {
+        fontSize: "42px",
+        fontStyle: "bold",
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+      }
+    ).setOrigin(0.5);
+
+    //
+    // Loading text
+    //
+    const loadingText = this.add.text(
+      0,
+      70,
+      "Loading... 0%",
+      {
+        fontSize: "20px",
+        color: "#ffffff",
+      }
+    ).setOrigin(0.5);
+
+    //
+    // Progress box
+    //
+    const box = this.add.graphics();
+
+    box.fillStyle(0xffffff, 0.20);
+    box.fillRoundedRect(
+      -170,
+      120,
+      340,
+      22,
+      11
+    );
+
+    //
+    // Progress bar
+    //
+    this.progressBar = this.add.graphics();
+
+    //
+    // Add everything to the container
+    //
+    ui.add([
+      logo,
+      title,
+      loadingText,
+      box,
+      this.progressBar,
+    ]);
+
+    //
+    // Update progress
+    //
+    this.load.on("progress", (value: number) => {
+
+      this.progressBar.clear();
+
+      this.progressBar.fillStyle(0xffffff);
+
+      this.progressBar.fillRoundedRect(
+        -165,
+        125,
+        330 * value,
+        12,
+        6
+      );
+
+      loadingText.setText(`Loading ${Math.round(value * 100)}%`);
+    });
+
+    this.load.on("complete", () => {
+      this.progressBar.destroy();
+      box.destroy();
+    });
+
+    //
+    // Load all game assets
+    //
     preloadSoundEffects(this);
+
+    if (!this.textures.exists(BLUE_SOLDIER_KEY)) {
+      this.load.image(BLUE_SOLDIER_KEY, blueSoldierUrl);
+    }
+
+    if (!this.textures.exists(RED_SOLDIER_KEY)) {
+      this.load.image(RED_SOLDIER_KEY, redSoldierUrl);
+    }
+
+    // Future assets...
+    // this.load.image(...)
+    // this.load.audio(...)
+    // this.load.atlas(...)
+    // this.load.spritesheet(...)
+  }
+
+  create() {
+    this.scene.start("MenuScene");
+  }
+}
+
+class MenuScene extends Phaser.Scene {
+  constructor() {
+    super('MenuScene');
   }
 
   create() {
@@ -515,10 +664,6 @@ class AiSetupScene extends Phaser.Scene {
 
   constructor() {
     super('AiSetupScene');
-  }
-
-  preload() {
-    preloadSoundEffects(this);
   }
 
   create() {
@@ -697,10 +842,6 @@ class CampaignLevelSelectScene extends Phaser.Scene {
 
   constructor() {
     super('CampaignLevelSelectScene');
-  }
-
-  preload() {
-    preloadSoundEffects(this);
   }
 
   create() {
@@ -918,10 +1059,6 @@ class OnlineLobbyScene extends Phaser.Scene {
 
   constructor() {
     super('OnlineLobbyScene');
-  }
-
-  preload() {
-    preloadSoundEffects(this);
   }
 
   create() {
@@ -1316,12 +1453,6 @@ class HexConquestScene extends Phaser.Scene {
 
   constructor() {
     super('HexConquestScene');
-  }
-
-  preload() {
-    this.load.image(BLUE_SOLDIER_KEY, blueSoldierUrl);
-    this.load.image(RED_SOLDIER_KEY, redSoldierUrl);
-    preloadSoundEffects(this);
   }
 
   init(data: {
@@ -2479,7 +2610,7 @@ const config: Phaser.Types.Core.GameConfig = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  scene: [MenuScene, AiSetupScene, CampaignLevelSelectScene, OnlineLobbyScene, HexConquestScene],
+  scene: [BootScene, PreloaderScene, MenuScene, AiSetupScene, CampaignLevelSelectScene, OnlineLobbyScene, HexConquestScene],
 };
 
 new Phaser.Game(config);
